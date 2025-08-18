6c0fb9ec963ad87719275813d31cd30ffe9aed11
Ed <108343328+eded001@users.noreply.github.com>
2025-08-17 23:39:05 -0300

    feat: setup react native

---
+++ app/.eslintrc.js
@@ -0,0 +1,7 @@
+module.exports = {
+  root: true,
+  extends: '@react-native-community',
+  parserOptions: {
+    requireConfigFile: false, // permite o ESLint funcionar mesmo se Babel falhar
+  },
+};
