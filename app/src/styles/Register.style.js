05cefbf312f81bb529842bbe8da1dbe2eeeb0633
YagoraDev <yagosiqueiraa1@gmail.com>
2025-09-02 13:19:37 -0300

    feat(auth): AMZNPJ02-12 Cria página de cadastro

--- app/src/styles/Register.style.js
+++ app/src/styles/Register.style.js
@@ -1,3 +1,51 @@
 import { StyleSheet } from 'react-native';
 
-export default style = StyleSheet.create();
+export default style = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#f2f2f2",
+    padding: 20,
+  },
+  title: {
+    fontSize: 28,
+    marginBottom: 20,
+    fontWeight: "bold",
+    color: "#333",
+  },
+  input: {
+    width: "100%",
+    height: 50,
+    backgroundColor: "#fff",
+    marginBottom: 15,
+    paddingHorizontal: 15,
+    borderRadius: 8,
+    borderWidth: 1,
+    borderColor: "#ccc",
+  },
+  button: {
+    width: "100%",
+    height: 50,
+    backgroundColor: "#007bff",
+    justifyContent: "center",
+    alignItems: "center",
+    borderRadius: 8,
+    marginBottom: 10,
+  },
+  buttonText: {
+    color: "#fff",
+    fontSize: 18,
+    fontWeight: "bold",
+  },
+  loginContainer: {
+    marginBottom: 10,
+    flexDirection: "row",
+  },
+  loginText: {
+    color: "#000",            
+    textDecorationLine: "underline", 
+    fontWeight: "bold",
+    fontSize: 15,
+  },
+});
